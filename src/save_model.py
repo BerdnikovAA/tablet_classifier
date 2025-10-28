c01f3972f382cbf80d29ede82848b4fcbbed84b9
Бердников Андрей Анатольевич <a.berdnikov@npovs.icl.kazan.ru>
2025-10-28 13:26:49 +0300

    Added own classifier class.

---
+++ src/save_model.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+
+import torch
+
+
+def save_model(state_dict, model_name):
+    path = 'models'
+    if not os.path.exists(path):
+        os.mkdir(path)
+    torch.save(state_dict, model_name)
